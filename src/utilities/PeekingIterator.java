486498b0b09652bf027f87016efc128f233cc331
Adithya <ra102@duke.edu>
2017-10-21 15:49:16 -0400

    Generic PeekingIterator class to support peeking operation for parsing, especially in handling variable no. of args

---
+++ src/utilities/PeekingIterator.java
@@ -0,0 +1,36 @@
+package utilities;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class PeekingIterator<E> {
+
+	private Iterator<E> it;
+	private E peekedElement;
+	
+	public PeekingIterator(Iterator<E> it) {
+		this.it = it;
+	}
+	
+	public E peek() throws NoSuchElementException {
+		if (peekedElement != null) {
+			return peekedElement;
+		}
+		peekedElement = it.next();
+		return peekedElement;
+	}
+	
+	public E next() throws NoSuchElementException {
+		if (peekedElement != null) {
+			E nextVal = peekedElement;
+			peekedElement = null;
+			return nextVal;
+		}
+		return it.next();
+	}
+	
+	public boolean hasNext() {
+		return it.hasNext();
+	}
+	
+}
