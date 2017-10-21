4872e1d428525f8232d7b349a7ef6e0e53f34a34
MATTHEW MOSCA <MJM141@duke.edu>
2017-10-21 16:46:04 -0400

    frontend package and frontend.turtle_display added, as well as TurtleView class within

---
+++ src/frontend/turtle_display/TurtleView.java
@@ -0,0 +1,28 @@
+package frontend.turtle_display;
+
+import apis.TurtleDisplay;
+
+public class TurtleView implements TurtleDisplay{
+	/**
+     * Move a turtle's image to a new location within the pane.
+     *
+     * @param turtleIndex - identifies which turtle within the turtle collection to move
+     * @param xCoordinate - the new x-coordinate of the turtle
+     * @param yCoordinate - the new y-coordinate of the turtle
+     */
+    public void move(int turtleIndex, double xCoordinate, double yCoordinate) {
+    	
+    }
+
+    /**
+     * Change the direction to which the turtle's image points. Since coordinates are handled in the backend, this
+     * direction only affects visualization (that is, the turtle only move in the direction it points to because of the
+     * angle as stored in the backend).
+     *
+     * @param turtleIndex - identifies which turtle within the turtle collection to rotate
+     * @param angle - the direction the turtle's image should point toward
+     */
+    public void rotate(int turtleIndex, double angle) {
+    	
+    }
+}
