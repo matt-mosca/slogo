1b628fa6d38762902763444149d3e0e6362a0db0
Adithya <ra102@duke.edu>
2017-10-22 15:18:52 -0400

    Update calls to FunctionStore methods to match refactor

--- src/backend/control_nodes/DataAccessingNode.java
+++ src/backend/control_nodes/DataAccessingNode.java
@@ -2,6 +2,7 @@ package backend.control_nodes;
 
 import backend.FunctionsStore;
 import backend.SyntaxNode;
+import backend.error_handling.UndefinedVariableException;
 
 public abstract class DataAccessingNode implements SyntaxNode {
 
@@ -12,7 +13,11 @@ public abstract class DataAccessingNode implements SyntaxNode {
     }
 
     protected double getVariableValue(String name) {
-        return store.getVariableValue(name);
+    		try {
+    	        return store.getVariableValue(name);    			
+    		} catch (UndefinedVariableException e) {
+    			return 0;
+    		}
     }
 
     protected double setVariable(String name, Double value) {
