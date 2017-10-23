f3be49862de3fbc26c0f8c4fb4711c70a314a31d
Ben Schwennesen <bas65@duke.edu>
2017-10-23 02:40:30 -0400

    Add Controller that will allow safer frontend/backend interactions (not yet implemented)

---
+++ src/backend/Controller.java
@@ -0,0 +1,17 @@
+package backend;
+
+import frontend.turtle_display.TurtleView;
+
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+
+public class Controller {
+
+    public Controller(TurtleView turtleView, Rectangle turtleField) {
+
+    }
+
+    public static void main(String[] args) {
+        Color c = new Color(0, 0 ,0, 0);
+    }
+}
