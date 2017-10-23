cb39df92af9f5c64dbf7302d5d3e2915b293427d
MATTHEW MOSCA <MJM141@duke.edu>
2017-10-22 23:30:23 -0400

    Allowed for change to drawing color

--- src/frontend/turtle_display/Drawer.java
+++ src/frontend/turtle_display/Drawer.java
@@ -1,9 +1,21 @@
 package frontend.turtle_display;
 
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Line;
 
 public class Drawer {
+	private Paint drawColor;
+	
+	public Drawer() { 
+		drawColor = Color.BLACK;
+	}
+	
+	public void changeDrawColor(Paint color) {
+		drawColor = color;
+	}
+	
 	public void drawLine(double startX, double startY, double endX, double endY, Pane layout) {
 		Line lineToDraw = new Line(startX, startY, endX, endY);
 		layout.getChildren().add(lineToDraw);
