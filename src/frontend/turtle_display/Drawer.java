219ad3805dfdce3217923d0ee73a30fee17e5546
Ben Schwennesen <bas65@duke.edu>
2017-10-25 01:53:50 -0400

    Add a pen width field

--- src/frontend/turtle_display/Drawer.java
+++ src/frontend/turtle_display/Drawer.java
@@ -7,10 +7,13 @@ import javafx.scene.shape.Line;
 
 public class Drawer {
 	private Paint drawColor;
+	private double strokeWidth;
 	private boolean isPenDown;
 	
 	public Drawer() { 
 		drawColor = Color.BLACK;
+		// todo - prob make this not a magic number (temp)
+		strokeWidth = 1.0;
 		isPenDown = true;
 	}
 	
@@ -18,11 +21,16 @@ public class Drawer {
 		drawColor = color;
 		isPenDown = down;
 	}
+
+	public void setDrawColor(Paint color) { drawColor = color; }
+
+	public void setStrokeWidth(double width) { strokeWidth = width; }
 	
 	public void drawLine(double startX, double startY, double endX, double endY, Pane layout) {
 		if(isPenDown) {
 			Line lineToDraw = new Line(startX, startY, endX, endY);
 			lineToDraw.setStroke(drawColor);
+			lineToDraw.setStrokeWidth(strokeWidth);
 			layout.getChildren().add(lineToDraw);
 			System.out.println(drawColor);
 		}
