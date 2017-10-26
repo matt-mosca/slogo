b9dc3d6dc3df582e792789568ea8b7eb2de5b156
MATTHEW MOSCA <MJM141@duke.edu>
2017-10-26 17:01:28 -0400

    Small changes to pen display

--- src/frontend/turtle_display/Drawer.java
+++ src/frontend/turtle_display/Drawer.java
@@ -28,9 +28,8 @@ public class Drawer {
 	public void drawLine(double startX, double startY, double endX, double endY, Pane layout) {
 		if(isPenDown) {
 			Line lineToDraw = new Line(startX, startY, endX, endY);
-			//lineToDraw.setStrokeWidth(strokeWidth);
-			//TODO - line above doesn't work, no idea why
 			lineToDraw.setStroke(drawColor);
+			lineToDraw.setStrokeWidth(strokeWidth);
 			layout.getChildren().add(lineToDraw);
 			System.out.println(drawColor);
 		}
