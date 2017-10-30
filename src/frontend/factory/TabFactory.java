81e16d630561b0c0042ddd85cb134c829189d938
Tyler Yam <tjy8@duke.edu>
2017-10-30 16:25:57 -0400

    created TabPane for variation and function

--- src/frontend/factory/TabFactory.java
+++ src/frontend/factory/TabFactory.java
@@ -1,5 +1,13 @@
 package frontend.factory;
 
-public class TabFactory {
+import javafx.scene.Group;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
+public class TabFactory {
+	public void makeTab(String name, Group root, TabPane tabPane) {
+		Tab tab = new Tab(name);
+		tab.setContent(root);
+		tabPane.getTabs().add(tab);
+	}
 }
