184943773e8df104d8a511efd22144facb22c13c
Tyler Yam <tjy8@duke.edu>
2017-10-29 22:25:23 -0400

    Added multiple window workspace functionality

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,52 @@
 package main;
 
+import frontend.factory.ButtonFactory;
+import frontend.factory.TextFieldFactory;
 import frontend.window_setup.IDEWindow;
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	public static final String APP_NAME = "SLogo";
-	
+	private static final Paint STANDARD_AREA_COLOR = Color.AQUA;
+	public static final double TOTAL_WIDTH = 400;
+	public static final double TOTAL_HEIGHT = 400;
+	private ButtonFactory buttonMaker = new ButtonFactory();
+	private TextFieldFactory textFieldMaker = new TextFieldFactory();
 	private Stage primaryStage;
-	
+	private Group root = new Group();
+	private HBox hbox = new HBox();
+	private String stageName = "SLogo";
+	private TextField textField;
+	private Scene primaryScene;
 	public void start(Stage primary) {
 		primaryStage = primary;
 		primaryStage.setTitle(APP_NAME);
 		primaryStage.setResizable(false);
 		primaryStage.show();
-		IDEWindow window = new IDEWindow(primaryStage);
+		buttonMaker.makeGUIItem(e->createIDEWindow(), root, "Create SLogo Workspace");
+		textField = textFieldMaker.makeReturnableTextField(e->setStageName(), root, "Name SLogo Workspace");
+		hbox.getChildren().addAll(root.getChildren());
+		primaryScene = new Scene(hbox, TOTAL_WIDTH, TOTAL_HEIGHT, STANDARD_AREA_COLOR);
+		primaryStage.setScene(primaryScene);
+	}
+	private void setStageName()
+	{
+		stageName = textField.getText();
+	}
+
+	private void createIDEWindow() {
+		Stage newStage = new Stage();
+		newStage.setTitle(stageName);
+		newStage.setResizable(false);
+		newStage.show();
+		IDEWindow window = new IDEWindow(newStage);
 		window.setUpWindow();
 	}
 	
