e3fee41c909437d73d48fef3c9dd64657bfd4981
Adithya <ra102@duke.edu>
2017-10-31 14:18:25 -0400

    Add Javadocs for TurtleController

--- src/backend/turtle/TurtleController.java
+++ src/backend/turtle/TurtleController.java
@@ -12,11 +12,18 @@ import java.util.Set;
 import java.util.function.IntToDoubleFunction;
 import java.util.stream.Collectors;
 
+/**
+ * Top-level entity for handling turtle-related backend operations, both from
+ * commands and direct front end requests (event-handlers from mouse clicks,
+ * button presses, etc)
+ * 
+ * @author Ben Schwennesen
+ * @author Adithya Raghunathan
+ */
+
 public class TurtleController {
 
 	private TurtleView turtleView;
-	private double xBounds;
-	private double yBounds;
 
 	private List<Turtle> createdTurtles;
 
@@ -30,10 +37,16 @@ public class TurtleController {
 	// For O(1) checking of duplicates while preserving insertion order
 	private LinkedHashSet<Integer> toldTurtleIds;
 
+	/**
+	 * Initialize TurtleController with reference to turtleView (class implementing
+	 * TurtleDisplay), as well as x and y bounds for turtle
+	 * 
+	 * @param turtleView
+	 * @param xBounds
+	 * @param yBounds
+	 */
 	public TurtleController(TurtleView turtleView, double xBounds, double yBounds) {
 		this.turtleView = turtleView;
-		this.xBounds = xBounds;
-		this.yBounds = yBounds;
 		createdTurtles = new ArrayList<>();
 		Turtle firstTurtle = new Turtle();
 		activeTurtleId = FIRST_TURTLE_ID;
@@ -45,6 +58,12 @@ public class TurtleController {
 		initializeFirstTurtle();
 	}
 
+	/**
+	 * Add as many turtles as needed from current max turtleId to given turtleId
+	 * 
+	 * @param turtleId
+	 *            the id of the turtle up to which turtles must be added
+	 */
 	// TELL [ 100 ] -- creates all turtles up to 100
 	public void addTurtles(int turtleId) {
 		for (int newTurtleId = createdTurtles.size() + 1; newTurtleId <= turtleId; newTurtleId++) {
@@ -53,15 +72,148 @@ public class TurtleController {
 			turtleView.addTurtle();
 			final int TURTLE_INDEX = getZeroBasedId(newTurtleId);
 			turtleView.selectTurtleOnClick(TURTLE_INDEX, e -> {
-				setActiveTurtles(new Integer[] { TURTLE_INDEX + 1  });
+				setActiveTurtles(new Integer[] { TURTLE_INDEX + 1 });
 			});
-			
+
 		}
 	}
 
+	/**
+	 * 
+	 * @return total number of turtles created
+	 */
 	// TURTLES
 	public int getNumberTurtlesCreated() {
 		return createdTurtles.size();
+	}
+
+	/**
+	 * Return a list of the currently told turtle ids, distinct from backing store
+	 * Defensively Copied
+	 * 
+	 * @return
+	 */
+	public List<Integer> getToldTurtleIds() {
+		return toldTurtleIds.stream().mapToInt(id -> getZeroBasedId(id)).boxed().collect(Collectors.toList());
+	}
+
+	/**
+	 * Set the image of the turtle of the given index to the given image
+	 * 
+	 * @param index
+	 * @param image
+	 * @return index
+	 */
+	public double setTurtleImage(int index, Image image) {
+		turtleView.changeImage(index, image);
+		return index;
+	}
+
+	/**
+	 * Reset state to starting values
+	 */
+
+	// for undo/redo
+	public void clear() {
+		createdTurtles.clear();
+		activeTurtleId = FIRST_TURTLE_ID;
+		queryTurtleId = 0;
+		createdTurtles.add(new Turtle());
+		toldTurtleIds.clear();
+		toldTurtleIds.add(activeTurtleId);
+		turtleView.clear();
+		initializeFirstTurtle();
+	}
+
+	/**
+	 * Set the active list of turtles to the specified array
+	 * 
+	 * @param ids
+	 *            the new array of active turtles, 1-indexed
+	 * @return
+	 */
+	public double setActiveTurtles(Integer[] ids) {
+		toldTurtleIds.clear();
+		toldTurtleIds.addAll(Arrays.asList(ids));
+		for (int turtleId : toldTurtleIds) {
+			addTurtles(turtleId);
+		}
+		activeTurtleId = ids.length > 0 ? ids[ids.length - 1] : 0;
+		// TODO - Inform the front end of the new list of told turtles
+		turtleView.changeRepresentationOfActive(getToldTurtleIds());
+		return activeTurtleId;
+	}
+
+	/**
+	 * Set the pen down for the turtle of the specified 1-indexed index
+	 * 
+	 * @param index
+	 * @return 1 since pen is now down
+	 */
+
+	public double setPenDown(int index) {
+		Turtle turtle = getTurtle(index);
+		// TODO - should this need index as arguments too?
+		turtle.setPenUp(false);
+		turtleView.putDownPen(getZeroBasedId(index));
+		return 1;
+	}
+
+	/**
+	 * Set the pen up for the turtle of the specified 1-indexed index
+	 * 
+	 * @param index
+	 * @return 0 since pen is now up
+	 */
+	public double setPenUp(int index) {
+		Turtle turtle = getTurtle(index);
+		// TODO - should this need index as arguments too?
+		turtle.setPenUp(true);
+		turtleView.pickUpPen(getZeroBasedId(index));
+		return 0;
+	}
+
+	/**
+	 * Move the current set of active turtles forward by the specified number of
+	 * pixels
+	 * 
+	 * @param pixels
+	 *            number of pixels to move
+	 * @return the specified number of pixels
+	 */
+	// NOTE : Made public to support Controller
+	public double moveCurrentTurtlesForward(double pixels) {
+		return doForToldTurtles(turtleId -> {
+			addTurtles(turtleId);
+			return turtleMover.moveTurtleForward(getZeroBasedId(turtleId), pixels);
+		});
+	}
+
+	/**
+	 * Rotate the current set of active turtles forward by the specified number of
+	 * pixels in the specified direction
+	 * 
+	 * @param clockwise whether the rotation is clockwise or not
+	 * @param angleInDegrees the angle to rotate by
+	 * @return the specified angle
+	 */
+	// NOTE : Made public to support Controller
+	public double rotateCurrentTurtles(boolean clockwise, double angleInDegrees) {
+		return doForToldTurtles(
+				turtleId -> turtleRotater.rotateTurtle(getZeroBasedId(turtleId), clockwise, angleInDegrees));
+	}
+
+	/**
+	 * Query whether the pen of the specified turtle is up or down
+	 * @param index 1-indexed id of turtle
+	 * @return 1 if pen down, 0 if pen up
+	 */
+	public double isPenDown(int index) {
+		return getTurtle(index).isPenUp() ? 0 : 1;
+	}
+
+	double setCurrentTurtlesPenDown() {
+		return doForToldTurtles(turtleId -> setPenDown(turtleId));
 	}
 
 	// ASK [ turtle(s) ]
@@ -110,48 +262,9 @@ public class TurtleController {
 		return true;
 	}
 
-	private double doForTurtles(IntToDoubleFunction forEachTurtle, Collection<Integer> turtles) {
-		double result = 0;
-		for (int toldTurtleId : turtles) {
-			activeTurtleId = toldTurtleId;
-			result = forEachTurtle.applyAsDouble(toldTurtleId);
-		}
-		return result;
-	}
-
-	private double doForToldTurtles(IntToDoubleFunction forEachTurtle) {
-		return doForTurtles(forEachTurtle, toldTurtleIds);
-	}
-
-	public double setActiveTurtles(Integer[] ids) {
-		toldTurtleIds.clear();
-		toldTurtleIds.addAll(Arrays.asList(ids));
-		for (int turtleId : toldTurtleIds) {
-			addTurtles(turtleId);
-		}
-		activeTurtleId = ids.length > 0 ? ids[ids.length - 1] : 0;
-		// TODO - Inform the front end of the new list of told turtles
-		turtleView.changeRepresentationOfActive(getToldTurtleIds());	
-		return activeTurtleId;
-	}
-
 	// Return a copy, both as a snapshot and for security
 	Set<Integer> getToldTurtles() {
 		return new LinkedHashSet<>(toldTurtleIds);
-	}
-
-	// NOTE : Made public to support Controller
-	public double moveCurrentTurtlesForward(double pixels) {
-		return doForToldTurtles(turtleId -> {
-			addTurtles(turtleId);
-			return turtleMover.moveTurtleForward(getZeroBasedId(turtleId), pixels);
-		});
-	}
-
-	// NOTE : Made public to support Controller
-	public double rotateCurrentTurtles(boolean clockwise, double angleInDegrees) {
-		return doForToldTurtles(
-				turtleId -> turtleRotater.rotateTurtle(getZeroBasedId(turtleId), clockwise, angleInDegrees));
 	}
 
 	double setCurrentTurtlesHeading(double angleInDegrees) {
@@ -164,26 +277,6 @@ public class TurtleController {
 
 	double setCurrentTurtlesXY(double x, double y) {
 		return doForToldTurtles(turtleId -> turtleMover.setXY(getZeroBasedId(turtleId), x, y));
-	}
-
-	public double setPenDown(int index) {
-		Turtle turtle = getTurtle(index);
-		// TODO - should this need index as arguments too?
-		turtle.setPenUp(false);
-		turtleView.putDownPen(getZeroBasedId(index));
-		return 1;
-	}
-
-	double setCurrentTurtlesPenDown() {
-		return doForToldTurtles(turtleId -> setPenDown(turtleId));
-	}
-
-	public double setPenUp(int index) {
-		Turtle turtle = getTurtle(index);
-		// TODO - should this need index as arguments too?
-		turtle.setPenUp(true);
-		turtleView.pickUpPen(getZeroBasedId(index));
-		return 0;
 	}
 
 	double setCurrentTurtlesPenUp() {
@@ -238,10 +331,6 @@ public class TurtleController {
 		return heading(getQueryTurtleId());
 	}
 
-	public double isPenDown(int index) {
-		return getTurtle(index).isPenUp() ? 0 : 1;
-	}
-
 	double isCurrentTurtlesPenDown() {
 		return isPenDown(getQueryTurtleId());
 	}
@@ -252,6 +341,19 @@ public class TurtleController {
 
 	double isCurrentTurtlesShowing() {
 		return isShowing(getQueryTurtleId());
+	}
+
+	private double doForTurtles(IntToDoubleFunction forEachTurtle, Collection<Integer> turtles) {
+		double result = 0;
+		for (int toldTurtleId : turtles) {
+			activeTurtleId = toldTurtleId;
+			result = forEachTurtle.applyAsDouble(toldTurtleId);
+		}
+		return result;
+	}
+
+	private double doForToldTurtles(IntToDoubleFunction forEachTurtle) {
+		return doForTurtles(forEachTurtle, toldTurtleIds);
 	}
 
 	private double toggleTurtleShow(int index, boolean showing) {
@@ -278,26 +380,4 @@ public class TurtleController {
 				e -> setActiveTurtles(new Integer[] { FIRST_TURTLE_ID }));
 	}
 
-	public List<Integer> getToldTurtleIds() {
-		return toldTurtleIds.stream()
-				.mapToInt(id -> getZeroBasedId(id))
-				.boxed().collect(Collectors.toList());
-	}
-
-	public double setTurtleImage(int index, Image image) {
-		turtleView.changeImage(index, image);
-		return index;
-	}
-
-	// for undo/redo
-	public void clear() {
-		createdTurtles.clear();
-		activeTurtleId = FIRST_TURTLE_ID;
-		queryTurtleId = 0;
-		createdTurtles.add(new Turtle());
-		toldTurtleIds.clear();
-		toldTurtleIds.add(activeTurtleId);
-		turtleView.clear();
-		initializeFirstTurtle();
-	}
 }
